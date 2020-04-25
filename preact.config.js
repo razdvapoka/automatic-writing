3ffc9b2089a97d3e47440b9567453c517ea3700e
razdvapoka <razdvapoka@gmail.com>
2020-04-25 21:14:09 +0300

    :arrow_up: Upgrade preact-cli, add tailwind

---
+++ preact.config.js
@@ -0,0 +1,9 @@
+export default {
+  webpack(config, env, helpers, options) {
+    const stylusRule = config.module.rules.find(
+      rule => String(rule.test) === String(/\.styl$/),
+    );
+    stylusRule.use[0].options.options.paths =
+      stylusRule.use[0].options.options.paths[0];
+  },
+};
