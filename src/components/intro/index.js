1bbc4a6e8b232973e74ddd89ac04cb89fd89845a
razdvapoka <razdvapoka@gmail.com>
2020-06-05 15:18:11 +0300

    :lipstick: Add fog shader

--- src/components/intro/index.js
+++ src/components/intro/index.js
@@ -14,14 +14,82 @@ const vertex = `
   }
 `;
 const fragment = `
-  precision highp float;
-  uniform float uTime;
-  uniform vec3 uColor;
-  varying vec2 vUv;
-  void main() {
-    gl_FragColor.rgb = 0.5 + 0.3 * cos(vUv.xyx + uTime) + uColor;
-    gl_FragColor.a = 1.0;
-  }
+#ifdef GL_ES
+precision mediump float;
+#endif
+
+varying vec2 vUv;
+uniform float uTime;
+
+float random (in vec2 _st) {
+    return fract(sin(dot(_st.xy,
+                         vec2(12.9898,78.233)))*
+        43758.5453123);
+}
+
+// Based on Morgan McGuire @morgan3d
+// https://www.shadertoy.com/view/4dS3Wd
+float noise (in vec2 _st) {
+    vec2 i = floor(_st);
+    vec2 f = fract(_st);
+
+    // Four corners in 2D of a tile
+    float a = random(i);
+    float b = random(i + vec2(1.0, 0.0));
+    float c = random(i + vec2(0.0, 1.0));
+    float d = random(i + vec2(1.0, 1.0));
+
+    vec2 u = f * f * (3.0 - 2.0 * f);
+
+    return mix(a, b, u.x) +
+            (c - a)* u.y * (1.0 - u.x) +
+            (d - b) * u.x * u.y;
+}
+
+#define NUM_OCTAVES 3
+
+float fbm ( in vec2 _st) {
+    float v = 0.0;
+    float a = 0.5;
+    vec2 shift = vec2(100.0);
+    for (int i = 0; i < NUM_OCTAVES; ++i) {
+        v += a * noise(_st);
+        _st =
+        _st * 2.0
+        + shift
+        ;
+        a *= 0.5;
+    }
+    return v;
+}
+
+void main() {
+    float cols = 2.0;
+    vec2 st = vUv * cols;
+    float speedCoeff = 0.3;
+
+    vec3 color = vec3(0.0);
+
+    vec2 q = vec2(0.0);
+    q.x = fbm(st + speedCoeff * uTime);
+    q.y = fbm(st + vec2(1.0));
+
+    vec2 r = vec2(0.0);
+    float rTimeCoeff = 0.15;
+    float qCoeff = 1.0;
+    r.x = fbm(st + qCoeff * q + rTimeCoeff * uTime);
+    r.y = fbm(st + qCoeff * q + rTimeCoeff * uTime);
+
+    float f = fbm(st + r);
+
+    vec3 color1 = vec3(0.2, 0.2, 0.2);
+    vec3 color2 = vec3(1.0, 1.0, 1.0);
+    color = mix(color1,
+                color2,
+                clamp(f * 3.0, 0.0, 1.0));
+
+    gl_FragColor = vec4(f * color, 1.0);
+}
 `;
 
 const initShader = () => {
