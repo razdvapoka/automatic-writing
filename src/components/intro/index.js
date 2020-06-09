5fc8942f1f408fa0db06a0e981c57209e88f669d
razdvapoka <razdvapoka@gmail.com>
2020-06-09 15:07:49 +0300

    :lipstick: Fixed header

--- src/components/intro/index.js
+++ src/components/intro/index.js
@@ -7,27 +7,37 @@ import styles from "./styles.styl";
 
 const Intro = () => {
   return (
-    <div id="intro" class={cn(styles.intro, "pt-4 px-4 relative pb-10")}>
+    <div id="intro" class={cn(styles.intro, "pt-4 relative pb-10")}>
       <Fog canvasId="canvas" parentId="intro" />
-      <header class="flex justify-between xs uppercase">
+      <header
+        class={cn("z-50 fixed w-full px-4 flex justify-between xs uppercase", styles.fixedHeader)}
+      >
         <div class="w-1/3">
-          <Link class={cn("fixed z-20", styles.title)} href="/">
-            automatic writing
-          </Link>
+          <Link href="/">automatic writing</Link>
         </div>
         <ul class="w-5/12">
           <li>Britanka [critical & contextual studies]</li>
+        </ul>
+        <div class="w-1/6">
+          <ul>
+            <li>Autumn + Winter 2019/2020</li>
+          </ul>
+        </div>
+        <div class="w-1/12" />
+      </header>
+      <div class={cn("px-4 flex justify-between xs uppercase", styles.secondHeader)}>
+        <div class="w-1/3"></div>
+        <ul class="w-5/12">
           <li>BA(Hons) Graphic Design</li>
           <li>British higher school of art & design</li>
         </ul>
         <div class="w-1/6">
           <ul>
-            <li>Autumn + Winter 2019/2020</li>
             <li>Semester A</li>
           </ul>
         </div>
         <div class="w-1/12" />
-      </header>
+      </div>
       <div class="flex justify-end">
         <p class={cn("w-7/12 xm mt-9 text-darkgrey", styles.description)}>
           A semester-long exploration of the realtionship between language, media and design.
